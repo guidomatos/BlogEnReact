019a188f1259c25b921ecc05798aae1b10f7ee99
guidomatos <guido.matos.88@gmail.com>
2021-09-14 20:02:49 -0500

    1ra clase del taller

---
+++ src/bases/const-let.js
@@ -0,0 +1,28 @@
+
+    // Variables y constantes
+
+    const nombre = "Guido";
+    const apellido = "Matos";
+
+    var edad = 0;
+
+    const anioNacimiento = 1988;
+    const anioActual = 2021;
+
+    if (anioActual === 2021) {
+
+
+            let edad = 0;
+            edad = anioActual - anioNacimiento;
+
+            console.log('Edad actual entorno del if:', edad);
+
+
+    } else {
+
+
+    }
+
+
+    console.log('Edad actual entorno global:', edad);
+    // console.log('Edad 2 actual:', edad2);
